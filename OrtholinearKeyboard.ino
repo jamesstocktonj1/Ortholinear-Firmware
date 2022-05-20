336e58e98749ee7d0d6f4915c72a2839d47a1a2e
js3g20 <js3g20@soton.ac.uk>
2022-05-21 00:50:30 +0100

    Update OrtholinearKeyboard.ino

--- OrtholinearKeyboard.ino
+++ OrtholinearKeyboard.ino
@@ -1,135 +1,137 @@
 #include "Adafruit_TinyUSB.h"
+#include "pins.h"
+//#include "keys.h"
 
+
+// usb hid setup
 void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize);
-uint8_t read_buttons(void);
-
 uint8_t const desc_hid_report[] = {
   TUD_HID_REPORT_DESC_KEYBOARD()
 };
 
-Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD, 2, false);
+Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD);
 
 
-//uint8_t keys[6] = {HID_KEY_A, 0, 0, 0, 0, 0};
-//uint8_t keys[6] = {HID_KEY_GUI_LEFT, 0, 0, 0, 0, 0};
+// keyboard multiplexing functions
+void init_pins(void);
+uint16_t read_columns(uint8_t rowIndex);
+uint8_t read_keys(void);
 
-uint8_t keys[2] = {HID_KEY_CAPS_LOCK, HID_KEY_2};
+void print_keys(void);
 
-uint8_t pinState, npinState;
+
+
+// key status arrays
+uint16_t key_status[4] = {0, };
+uint16_t key_previous[4] = {0, };
+uint16_t key_diff[4] = {0, };
+
+
 
 void setup() {
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-
-  digitalWrite(1, HIGH);
-  digitalWrite(2, HIGH);
-
-  pinState = read_buttons();
-
-  USBDevice.setManufacturerDescriptor("James Stockton");
-  //TinyUSBDevice.setManufacturerDescriptor("Hello");
-  USBDevice.setProductDescriptor("Ortholinear Keyboard");
   
-  usb_hid.setReportCallback(NULL, hid_report_callback);
-  usb_hid.begin();
-
-  while(!TinyUSBDevice.mounted()); 
+  init_pins();
 }
 
 void loop() {
-
-  npinState = read_buttons();
-
-
-  if(npinState ^ pinState) {
-    uint8_t buf[6] = {0, };
-    //uint8_t i = 0;
-
-    uint8_t pinDif = npinState ^ pinState;
-
-    Serial.print("Button Difference: ");
-    Serial.print(npinState ^ pinState, BIN);
-    Serial.print("\tButton State: ");
-    Serial.print(npinState);
-    Serial.print("\n");
-    /*
-    if((pinDif & (1 << 0)) && !(pinState & (1 << 0))) {
-      buf[i++] = keys[0];
-    }
-    if((pinDif & (1 << 1)) && !(pinState & (1 << 1))) {
-      buf[i++] = keys[1];
-    }
-    */
-    
-    if(TinyUSBDevice.suspended()) {
-      TinyUSBDevice.remoteWakeup();
-    }
-    
-    if((pinDif & ~pinState) & (1 << 0)) {
-      //usb_hid.keyboardPress(0, '1');
-      //buf[i++] = keys[0];
-      buf[0] = keys[0];
-      usb_hid.keyboardReport(0, 0, buf);
-    }
-    if((pinDif & ~pinState) & (1 << 1)) {
-      usb_hid.keyboardPress(0, '2');
-      //buf[i++] = keys[1];
-    }
-
-    if((pinDif & pinState) & 0b11) {
-      usb_hid.keyboardRelease(0);
-    }
-    
-    //usb_hid.keyboardReport(0, 0, buf);
-    //delay(10);
-    //usb_hid.keyboardRelease(0);
-
-    pinState = npinState;
+  
+  if(read_keys()) {
+    print_keys();
   }
-    
+  
   delay(20);
 }
 
-uint8_t read_buttons() {
-  uint8_t buttonState = 0;
 
-  digitalWrite(1, LOW);
-  digitalWrite(2, HIGH);
+void init_pins() {
 
-  if(digitalRead(0) == 0) {
-    buttonState += 1;
+  // itterate through rows
+  for(int i=0; i<4; i++) {
+    pinMode(rows[i], OUTPUT);
+    digitalWrite(rows[i], HIGH);
   }
 
-
-  digitalWrite(1, HIGH);
-  digitalWrite(2, LOW);
-
-  if(digitalRead(0) == 0) {
-    buttonState += 2;
+  // itterate through button columns
+  for(int j=0; j<12; j++) {
+    pinMode(button_columns[j], INPUT_PULLUP);
   }
 
-
-  digitalWrite(1, HIGH);
-  digitalWrite(2, HIGH);
-  
-  return buttonState;
+  // itterate through led columns
+  for(int k=0; k<10; k++) {
+    pinMode(led_columns[k], OUTPUT);
+    pinMode(led_columns[k], LOW);
+  }
 }
 
-void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+uint16_t read_column(uint8_t rowIndex) {
 
-  (void) report_id;
-  (void) bufsize;
+  uint16_t columnStatus = 0;
 
-  uint8_t temp = buffer[0];
+  // set row values (all 1 except rowIndex)
+  for(int i=0; i<4; i++) {
+    digitalWrite(rows[i], i!=rowIndex);
+  }
 
-  Serial.print("Report: ");
-  Serial.print(report_id);
-  Serial.print("\tSize: ");
-  Serial.print(bufsize);
-  Serial.print("\n");
+  // itterate through columns
+  for(int j=0; j<12; j++) {
 
-  digitalWrite(LED_BUILTIN, temp & (1 << 1));
+    // read value and add to status
+    if(digitalRead(button_columns[j]) == 0) {
+      columnStatus += (1 << j);
+    }
+    //columnStatus += (digitalRead(button_columns[j]) * (1 << j));
+  }
+
+  // reset row values
+  for(int i=0; i<4; i++) {
+    digitalWrite(rows[i], HIGH);
+  }
+
+  return columnStatus;
+}
+
+uint8_t read_keys() {
+
+  uint16_t temp;
+  uint8_t change = 0;
+
+  // itterate through rows
+  for(int i=0; i<4; i++) {
+    
+    // set previous key value
+    key_previous[i] = key_status[i];
+
+    // set new key values
+    temp = read_column(i);
+    key_status[i] = temp;
+    key_diff[i] = temp ^ key_previous[i];
+
+    // set change flag
+    if(temp) {
+      change = 1;
+    }
+
+    delay(2);
+  }
+
+  return change;
+}
+
+void print_keys() {
+
+  // itterate through rows
+  for(int i=0; i<4; i++) {
+
+    // itterate through columns
+    for(int j=0; j<12; j++) {
+
+      if(key_status[i] & key_diff[i] & (1 << j)) {
+        Serial.print("Button Pressed: row ");
+        Serial.print(i);
+        Serial.print(" columns ");
+        Serial.print(j);
+        Serial.print("\n");
+      }
+    }
+  }
 }
