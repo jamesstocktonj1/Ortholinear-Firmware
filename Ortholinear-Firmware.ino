90a804520d4875f5eab3d6e15483bd892a97a50d
js3g20 <js3g20@soton.ac.uk>
2022-06-11 23:17:31 +0100

    Update Ortholinear-Firmware.ino

--- Ortholinear-Firmware.ino
+++ Ortholinear-Firmware.ino
@@ -126,18 +126,18 @@ void init_pins() {
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
     pinMode(rows[i], OUTPUT);
     digitalWrite(rows[i], HIGH);
   }
 
-  // itterate through button columns
+  // iterate through button columns
   for(int j=0; j<12; j++) {
     pinMode(button_columns[j], INPUT_PULLUP);
   }
 
-  // itterate through led columns
+  // iterate through led columns
   for(int k=0; k<10; k++) {
     pinMode(led_columns[k], OUTPUT);
     pinMode(led_columns[k], LOW);
@@ -153,7 +153,7 @@ uint16_t read_column(uint8_t rowIndex) {
     digitalWrite(rows[i], i!=rowIndex);
   }
 
-  // itterate through columns
+  // iterate through columns
   for(int j=0; j<12; j++) {
 
     // read value and add to status
@@ -176,7 +176,7 @@ uint8_t read_keys() {
   uint16_t temp;
   uint8_t change = 0;
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
     
     // set previous key value
@@ -203,10 +203,10 @@ uint8_t read_keys() {
 
 void print_keys() {
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
 
-    // itterate through columns
+    // iterate through columns
     for(int j=0; j<12; j++) {
 
       if(key_status[i] & key_diff[i] & (1 << j)) {
@@ -244,10 +244,10 @@ void send_keys() {
   uint8_t curLayer = key_status[3] & (1 << 6);
 
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
 
-    // itterate through columns
+    // iterate through columns
     for(int j=0; j<12; j++) {
 
       uint8_t buttonCode = 0;
@@ -361,7 +361,7 @@ void send_keys() {
 
 void write_leds() {
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
 
     // set row values (all 1 except rowIndex)
@@ -369,7 +369,7 @@ void write_leds() {
       digitalWrite(rows[k], k!=i);
     }
 
-    // itterate through columns
+    // iterate through columns
     for(int j=0; j<10; j++) {
 
       digitalWrite(led_columns[i], led_state[i] & (1 << j));
@@ -391,7 +391,7 @@ void write_leds() {
 
 void set_led_states() {
 
-  // itterate through rows
+  // iterate through rows
   for(int i=0; i<4; i++) {
 
     led_state[i] = key_status[i];
